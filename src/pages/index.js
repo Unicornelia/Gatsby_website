c0b532aeb30170c5419d9604fc2849e556722a15
Kornelia Szabo <kszabo@thoughtworks.com>
2019-05-15 12:16:19 +0200

    Rewriting template index for actual content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react"
+import Header from "../components/header"
+import Layout from "../components/layout"
+import Footer from "../components/footer"
+import StyledMap from "../components/styledMap"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <>
+    <Layout>
+      <Header
+        headerText="Szita Bernadett"
+        subText="Klinikai szakpszichológus, relaxációs- és szimbólumterapeuta"
+        headerPadding="10rem"
+      />
+      <StyledMap />
+      <Footer />
+    </Layout>
+  </>
+)
