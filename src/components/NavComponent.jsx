d1a0a7522b058f6ece73bbad018a0321c27b5aba
Kori <ez.kornelia@gmail.com>
2019-09-07 15:13:58 +0200

    Adding nav component

---
+++ src/components/NavComponent.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const ListLink = props => (
+  <li
+    style={{
+      display: `inline-block`,
+      marginRight: `1rem`,
+      marginBottom: `1rem`,
+    }}
+  >
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
+const NavComponent = () => (
+  <div style={{ padding: `1rem` }}>
+    <div className="left-nav">
+      <ul style={{ listStyle: `none`, float: `left` }}>
+        <ListLink to="/">Kezdőlap</ListLink>
+      </ul>
+    </div>
+    <div className="right-nav">
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/about/">Magamról</ListLink>
+        <ListLink to="/amibenSegithetek/">Amiben segithetek</ListLink>
+        <ListLink to="/noiEgeszseg/">Női egészségpszichológia</ListLink>
+        <ListLink to="/contact/">Kapcsolat</ListLink>
+      </ul>
+    </div>
+  </div>
+)
+
+export default NavComponent
